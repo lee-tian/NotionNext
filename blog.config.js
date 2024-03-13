e8a825d6419adeb8c5a9af7fae8f72cabe89617a
lee-tian <skywaytinker@gmail.com>
2024-03-13 10:26:44 -0700

    Update sit link

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Emily不拍糖水片', // 您的昵称 例如 Emily不拍糖水片
   BIO: process.env.NEXT_PUBLIC_BIO || '一对爱好摄影的情侣🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://emilydreamphoto.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://emilydream.life', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Emily不拍糖水片, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
